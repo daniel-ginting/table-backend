c43780020d5d3c21341f98cbe02e77c5f672a628
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-07 04:48:48 +0800

    asek

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// const knex = require("knex")({
-//   client: "pg",
-//   connection: {
-//     host: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   },
-// });
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+});
 const cors = require("cors");
 
 const app = express();
@@ -16,15 +16,19 @@ app.use(cors());
 
 let data;
 
-// knex
-//   .select("*")
-//   .from("students")
-//   .then((res) => {
-//     data = res;
-//   });
+knex
+  .select("*")
+  .from("students")
+  .then((res) => {
+    data = res;
+  });
 
 app.get("/", (req, res) => {
   res.send('ok!');
 });
+
+// app.get("/", (req, res) => {
+//   res.send('ok!');
+// });
 
 app.listen(process.env.PORT || 4000);
