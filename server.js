5cceac6afb6e00819192c73c302c367f9950f925
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-07 18:19:18 +0800

    add feature

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const knex = require("knex")({
   client: "pg",
   connection: {
@@ -14,15 +15,31 @@ const app = express();
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   knex
-  .select("*")
-  .from("students")
-  .then((res2) => {
-    res.send(res2);
-  });
-  
+    .select("*")
+    .from("students")
+    .then((res2) => {
+      res.send(res2);
+    });
 });
 
+app.get("/students", (req, res) => {
+  // grab students
+  res.send(db);
+});
+
+app.post("/insert-student", (req, res) => {
+  // add student
+  knex("students")
+    .insert([
+      { fullname: req.body.fullname },
+      { dateofbirth: req.body.dateofbirth },
+    ])
+    .then(() => res.json("success adding student"));
+});
 
 app.listen(process.env.PORT || 4000);
+// app.listen(3000);
