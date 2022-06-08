1518f30d6c9057abc467b1bc2c15403e07270303
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-08 16:41:48 +0800

    asej

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://melodious-cobbler-d81d3d.netlify.app/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -29,7 +23,6 @@ app.get("/", (req, res) => {
 
 app.get("/students", (req, res) => {
   // grab students
-  res.set('Access-Control-Allow-Origin', '*');
   knex
     .select("*")
     .from("students")
@@ -40,7 +33,6 @@ app.get("/students", (req, res) => {
 
 app.post("/insert-student", (req, res) => {
   // add student
-  res.set('Access-Control-Allow-Origin', '*');
   knex("students")
     .insert({ fullname: req.body.fullname, dateofbirth: req.body.dateofbirth })
     .then(() => res.json("success adding student"));
