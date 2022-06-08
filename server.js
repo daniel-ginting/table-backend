2ccfcdc6a86bce77680554265b44008ea9e679fe
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-08 17:13:38 +0800

    asej

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Please use other URL parameter");
